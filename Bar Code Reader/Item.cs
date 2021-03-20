2a95952a5d7d49d29b5e3d0bea9adfbdd477000a
Braxton Shoop <braxton.shoop@gmail.com>
2021-03-20 16:16:04 -0500

    api get fetch added

---
+++ Bar Code Reader/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bar_Code_Reader
+{
+    public class Item
+    {
+            public Item(string title, string description, string brand)
+            {
+            Title = title;
+            Description = description;
+            Brand = brand;
+
+            }
+            //Your Properties are auto-implemented.
+            public string Title { get; set; }
+
+            public string Description { get; set; }
+
+            public string Brand { get; set; }
+    }
+}
